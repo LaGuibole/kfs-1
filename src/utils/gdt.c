2257d18322bdaf2f77cec47ab2f41617692521c3
CodeWithCharles <charles.probe@gmail.com>
2026-02-20 12:32:48 +0100

    ~ | Gdt at the correct address

--- src/utils/gdt.c
+++ src/utils/gdt.c
@@ -4,13 +4,13 @@
 
 extern void gdt_flush(u32 ptr);
 
-struct gdt_entry_struct gdt_entries[GDT_ENTRIES];
-struct gdt_ptr_struct   *gdt_ptr = (struct gdt_ptr_struct*)GDT_ADDRESS;
+struct gdt_entry_struct gdt_entries[GDT_ENTRIES] __attribute__((section(".gdt")));
+struct gdt_ptr_struct   gdt_ptr;
 
 void init_gdt()
 {
-	gdt_ptr->limit = (sizeof(struct gdt_entry_struct) * GDT_ENTRIES) - 1;
-	gdt_ptr->base = (u32)&gdt_entries;
+	gdt_ptr.limit = (sizeof(struct gdt_entry_struct) * GDT_ENTRIES) - 1;
+	gdt_ptr.base = (u32)&gdt_entries;
 
 	set_gdt_gate(0,0,0,0,0);            		// null segment - convention
 	set_gdt_gate(1,0,0xFFFFFFFF,0x9A,0xCF);		// kernel code segment
@@ -20,7 +20,7 @@ void init_gdt()
 	set_gdt_gate(5,0,0xFFFFFFFF,0xF2,0xCF);		// User data segment
 	set_gdt_gate(6,0,0xFFFFFFFF,0xF6,0xCF);		// User stack
 
-	gdt_flush((u32)gdt_ptr);
+	gdt_flush(((u32)&gdt_ptr));
 }
 
 void set_gdt_gate(u32 num, u32 base, u32 limit, u8 access, u8 gran)
@@ -40,7 +40,7 @@ void print_gdt_dump()
 {
     struct gdt_ptr_struct gdtr_value;
     __asm__ volatile ("sgdt %0" : "=m"(gdtr_value));
-    printk("GDT base: 0x%08x, limit: 0x%04x\n", KERNEL_PHYS(gdtr_value.base), gdtr_value.limit);
+    printk("GDT base: 0x%08x, limit: 0x%04x\n", gdtr_value.base, gdtr_value.limit);
 	u8 *gdt = (u8 *)gdtr_value.base;
     for (u16 i = 0; i < gdtr_value.limit + 1; i += 8) {
         printk("GDT[%02d]: ", i / 8);

11fbc3052ec9972514c8608ce1d7bafd2f9a753d
guphilip <guphilip@kfs1donotdelete.myguest.virtualbox.org>
2026-02-20 18:41:39 +0100

    ~ | fix a faire sur \n pour backspace

--- src/utils/handlers.c
+++ src/utils/handlers.c
@@ -21,7 +21,7 @@ void keyboard_handler()
     if (c != 0)
     {
         if (scancode >= KEY_F1 && scancode <= KEY_F10)
-            terminal_switch_tab(c % 0xFFFFFFF0 - 1);
+            terminal_switch_tab(c % 0xFFFFFFF0 - 1);    
         else if (c == '\b')
             terminal_backspace();
         else
